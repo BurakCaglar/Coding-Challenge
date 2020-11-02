9fabdd5ce8bc54bb2f67cb4881dc3ff71daa4feb
burakcaglar <burakcaglar@yandex.com>
2020-11-02 22:11:14 +0300

    first

---
+++ 10 Days Javascript/Day 5/TemplateLiteral.js
@@ -0,0 +1,8 @@
+function sides(literals, ...expressions) {
+    const [a, p] = expressions;
+    const root = Math.sqrt((p * p) - (16 * a));
+    let s1 = ((p + root) / 4);    
+    let s2 = ((p - root) / 4); 
+ 
+    return [s1, s2].sort();   
+}
