c01bd926610082848a4c8b1e173e2d94fb4814b5
burakcaglar <burakcaglar@yandex.com>
2020-11-02 15:34:11 +0300

    documentation has been added.

--- 10 Days Javascript/Day 0/DataTypes.js
+++ 10 Days Javascript/Day 0/DataTypes.js
@@ -1,15 +1,4 @@
-/**
-*   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
-*   Print three lines:
-*   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
-*   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
-*   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
-*
-*	Parameter(s):
-*   secondInteger - The string representation of an integer.
-*   secondDecimal - The string representation of a floating-point number.
-*   secondString - A string consisting of one or more space-separated words.
-**/
+
 function performOperation(secondInteger, secondDecimal, secondString) {
     // Declare a variable named 'firstInteger' and initialize with integer value 4.
     const firstInteger = 4;
@@ -21,7 +10,7 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     const firstString = 'HackerRank ';
     
     // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
-    console.log(firstInteger + +(secondInteger));
+    console.log(firstInteger + +(secondInteger)); /* https://tr.javascript.info/operators  -  https://plnkr.co/edit/?p=preview&preview  */
     
     // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
     console.log(firstDecimal + +secondDecimal);
